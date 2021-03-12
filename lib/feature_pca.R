083bda97d213b0f57f3a59f8dfb19df3ff7b9641
catherineygao <77375906+catherineygao@users.noreply.github.com>
2021-03-11 23:10:43 -0800

    updated PCA feature

--- lib/feature_pca.R
+++ lib/feature_pca.R
@@ -5,6 +5,5 @@ library(caret)
 
 feature_pca <- function(dat){
   pca <- preProcess(x = dat[, -6007], method = "pca", thresh = 0.95)
-  pca_feature <- predict(pca, dat[, -6007])
-  return(pca_feature)
+  return(pca)
 }
