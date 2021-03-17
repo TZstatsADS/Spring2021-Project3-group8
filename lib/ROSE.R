aa77dd98960ffa78eb48c261139c22bf70a4596e
catherineygao <77375906+catherineygao@users.noreply.github.com>
2021-03-17 00:53:22 -0700

    test RF_PCA_ROSE

--- lib/ROSE.R
+++ lib/ROSE.R
@@ -3,7 +3,6 @@
 ###########################################################
 
 use_rose <- function(dat_train){
-  set.seed(2020)
   model <- ROSE(label~., data = dat_train)$data
   return(model)
 }
